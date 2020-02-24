8591188ce89f364946911ed31498d4fcc1660950
Joao Guimaraes <jssgmrs22@gmail.com>
2020-02-24 16:12:02 +0000

    Testing HTTP sending

---
+++ ImageTracker/Assets/Scripts/TestSend.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TestSend : MonoBehaviour
+{
+    public Toggle ToogleOne;
+    public Toggle ToogleTwo;
+    public InputField InputText;
+    readonly string BaseUrl = "https://prod-42.westeurope.logic.azure.com:443/workflows/291bfb34679c4f789aaed9f65f10541c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=NcQujo3zMZPwZ-fl2jQSl9nl3guu29Frd2cnCA0TGyU";
+
+
+    public void Begin()
+    {
+        StartCoroutine(Upload());
+    }
+
+
+    IEnumerator Upload()
+    {
+
+        byte[] toogleOne = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
+        byte[] toogleTwo = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
+        byte[] inputText = Encoding.UTF8.GetBytes(InputText.text);
+        byte[] date = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormFileSection("ToogleOne", ToogleOne.isOn.ToString()));
+        formData.Add(new MultipartFormFileSection("ToogleTwo", ToogleOne.isOn.ToString()));
+        formData.Add(new MultipartFormFileSection("InputText", InputText.text));
+        formData.Add(new MultipartFormFileSection("Date", DateTime.Now.ToString()));
+
+        UnityWebRequest www = UnityWebRequest.Post(BaseUrl, formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+
+    }
+
+}
--- ImageTracker/Assets/Scripts/TestSend2.cs
+++ ImageTracker/Assets/Scripts/TestSend2.cs
@@ -6,19 +6,17 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class HTTPSend : MonoBehaviour
+public class TestSend2 : MonoBehaviour
 {
     public Toggle ToogleOne;
     public Toggle ToogleTwo;
     public InputField InputText;
     readonly string BaseUrl = "https://prod-42.westeurope.logic.azure.com:443/workflows/291bfb34679c4f789aaed9f65f10541c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=NcQujo3zMZPwZ-fl2jQSl9nl3guu29Frd2cnCA0TGyU";
 
-
     public void Begin()
     {
         var request = CreateApiPostRequest(BaseUrl, new ApiAuthenticateRequest { ToogleOne = ToogleOne.isOn.ToString(), ToogleTwo = ToogleOne.isOn.ToString(), InputText = InputText.text, Date = DateTime.Now.ToString() });
         request.SendWebRequest();
-        StartCoroutine(Upload());
     }
 
 
@@ -63,32 +61,6 @@ public class HTTPSend : MonoBehaviour
 
 
 
-    IEnumerator Upload()
-    {
-
-        byte[] toogleOne = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
-        byte[] toogleTwo = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
-        byte[] inputText = Encoding.UTF8.GetBytes(InputText.text);
-        byte[] date = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
-
-        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        formData.Add(new MultipartFormFileSection("ToogleOne", toogleOne));
-        formData.Add(new MultipartFormFileSection("ToogleTwo", toogleTwo));
-        formData.Add(new MultipartFormFileSection("InputText", inputText));
-        formData.Add(new MultipartFormFileSection("Date", date));
-
-        UnityWebRequest www = UnityWebRequest.Post(BaseUrl, formData);
-        yield return www.SendWebRequest();
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Debug.Log("Form upload complete!");
-        }
-
-    }
+  
 
 }
