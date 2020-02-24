883f4f9e18da4389714f43f1c39b3dbbbdd36a4f
Joao Guimaraes <jssgmrs22@gmail.com>
2020-02-24 16:14:20 +0000

    Revert

--- ImageTracker/Assets/Scripts/HTTPSend.cs
+++ ImageTracker/Assets/Scripts/HTTPSend.cs
@@ -66,10 +66,7 @@ public class HTTPSend : MonoBehaviour
     IEnumerator Upload()
     {
 
-        byte[] toogleOne = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
-        byte[] toogleTwo = Encoding.UTF8.GetBytes(ToogleOne.isOn.ToString());
-        byte[] inputText = Encoding.UTF8.GetBytes(InputText.text);
-        byte[] date = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+        string url = "https://prod-42.westeurope.logic.azure.com:443/workflows/291bfb34679c4f789aaed9f65f10541c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KrIctjAR-KhNmt6M-A5fXo_kfoGmk2C5aFeu_Y0oU38";
 
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
         formData.Add(new MultipartFormFileSection("ToogleOne", toogleOne));
